c3a0a50984d5c970fa2d8dfb093545ae2c2bc899
Martin STEFFEN <mail@tiph.io>
2019-01-13 15:51:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+	return res.status(200)
+		.set('Content-type', 'text/html')
+		.end('<strong>This is the master branch</strong>')
+})
+
+app.listen(8080)
+console.log('Application listening on port 8080.')
